dbb09a25effd2764db2dacd3e073012599cfe4aa
ijzm <ignacio.zevallos@hotmail.com>
2015-08-01 17:55:14 -0500

    fixed some sizes, button working, (logging "PLAY") TODO: MATH AND ALIGMENT

--- boot.js
+++ boot.js
@@ -1,34 +1,41 @@
 var boot = function(game){
-    console.log("boot");
+	console.log("boot");
 };
 
 
 boot.prototype = {
-    preload: function() {
-        //preloads the sprites
-        this.game.load.image('blue', 'assets/drops/always/blue.png');
-        this.game.load.image('red', 'assets/drops/always/red.png');
-        this.game.load.image('yellow', 'assets/drops/always/yellow.png');
-        this.game.load.image('lightblue', 'assets/drops/often/lightblue.png');
-        this.game.load.image('lightgreen', 'assets/drops/often/lightgreen.png');
-        this.game.load.image('pink', 'assets/drops/often/pink.png');
-        this.game.load.image('green	', 'assets/drops/uncommon/green.png');
-        this.game.load.image('orange', 'assets/drops/uncommon/orange.png');
-        this.game.load.image('purple', 'assets/drops/uncommon/purple.png');
-        this.game.load.image('black', 'assets/drops/hardlyever/black.png');
-        this.game.load.image('bomb', 'assets/drops/bonus/bomb.png');
-        this.game.load.image('catfeed', 'assets/drops/bonus/catfeed.png');
-        this.game.load.image('heart', 'assets/drops/bonus/heart.gif');
-        //backgrounds
-        this.game.load.image('bg', 'assets/bg.png');
-        this.game.load.image('bgmenu','assets/bgmenu.png');
-        //net
-        this.game.load.image('net', 'assets/net.png');
-    },
+	preload: function() {
+		//preloads the sprites
+		this.game.load.image('blue', 'assets/drops/always/blue.png');
+		this.game.load.image('red', 'assets/drops/always/red.png');
+		this.game.load.image('yellow', 'assets/drops/always/yellow.png');
+		this.game.load.image('lightblue', 'assets/drops/often/lightblue.png');
+		this.game.load.image('lightgreen', 'assets/drops/often/lightgreen.png');
+		this.game.load.image('pink', 'assets/drops/often/pink.png');
+		this.game.load.image('green	', 'assets/drops/uncommon/green.png');
+		this.game.load.image('orange', 'assets/drops/uncommon/orange.png');
+		this.game.load.image('purple', 'assets/drops/uncommon/purple.png');
+		this.game.load.image('black', 'assets/drops/hardlyever/black.png');
+		this.game.load.image('bomb', 'assets/drops/bonus/bomb.png');
+		this.game.load.image('catfeed', 'assets/drops/bonus/catfeed.png');
+		this.game.load.image('heart', 'assets/drops/bonus/heart.gif');
+		//backgrounds
+		this.game.load.image('bg', 'assets/bg.png');
+		this.game.load.image('bgmenu','assets/bgmenu.png');
+		//net
+		this.game.load.image('net', 'assets/net.png');
+		//buttons
+		this.game.load.image("playbutton", "assets/play.png");
+	},
 
-   create: function() {
-
-        this.game.add.sprite(0, 0, 'bgmenu');
-
-    }
+	create: function() {
+		this.game.add.sprite(0, 0, 'bgmenu');
+		
+		var play = this.game.add.button(550/2, 400/2, "playbutton", this.playTheGame,this);
+	},
+	
+	playTheGame: function(){
+		console.log("PLAY");
+		//this.game.state.start("TheGame");
+	}
 }
