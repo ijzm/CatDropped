746c3bd571a43a2d73f00fdadeae12920870de78
ijzm <ignacio.zevallos@hotmail.com>
2015-08-01 17:44:01 -0500

    WORKING!!! created title screen, TODO: Add functioning buttons

--- boot.js
+++ boot.js
@@ -1,29 +1,34 @@
+var boot = function(game){
+    console.log("boot");
+};
 
 
-function preload() {
-	//preloads the sprites
-	game.load.image('blue', 'assets/drops/always/blue.png');
-	game.load.image('red', 'assets/drops/always/red.png');
-	game.load.image('yellow', 'assets/drops/always/yellow.png');
-	game.load.image('lightblue', 'assets/drops/often/lightblue.png');
-	game.load.image('lightgreen', 'assets/drops/often/lightgreen.png');
-	game.load.image('pink', 'assets/drops/often/pink.png');
-	game.load.image('green	', 'assets/drops/uncommon/green.png');
-	game.load.image('orange', 'assets/drops/uncommon/orange.png');
-	game.load.image('purple', 'assets/drops/uncommon/purple.png');
-	game.load.image('black', 'assets/drops/hardlyever/black.png');
-	game.load.image('bomb', 'assets/drops/bonus/bomb.png');
-	game.load.image('catfeed', 'assets/drops/bonus/catfeed.png');
-	game.load.image('heart', 'assets/drops/bonus/heart.gif');
-	//backgrounds
-	game.load.image('bg', 'assets/bg.png');
-    game.load.image('bgmenu','assets/bgmenu.png');
-	//net
-	game.load.image('net', 'assets/net.png');
-}
+boot.prototype = {
+    preload: function() {
+        //preloads the sprites
+        this.game.load.image('blue', 'assets/drops/always/blue.png');
+        this.game.load.image('red', 'assets/drops/always/red.png');
+        this.game.load.image('yellow', 'assets/drops/always/yellow.png');
+        this.game.load.image('lightblue', 'assets/drops/often/lightblue.png');
+        this.game.load.image('lightgreen', 'assets/drops/often/lightgreen.png');
+        this.game.load.image('pink', 'assets/drops/often/pink.png');
+        this.game.load.image('green	', 'assets/drops/uncommon/green.png');
+        this.game.load.image('orange', 'assets/drops/uncommon/orange.png');
+        this.game.load.image('purple', 'assets/drops/uncommon/purple.png');
+        this.game.load.image('black', 'assets/drops/hardlyever/black.png');
+        this.game.load.image('bomb', 'assets/drops/bonus/bomb.png');
+        this.game.load.image('catfeed', 'assets/drops/bonus/catfeed.png');
+        this.game.load.image('heart', 'assets/drops/bonus/heart.gif');
+        //backgrounds
+        this.game.load.image('bg', 'assets/bg.png');
+        this.game.load.image('bgmenu','assets/bgmenu.png');
+        //net
+        this.game.load.image('net', 'assets/net.png');
+    },
 
-funcion create() {
-    
-    game.add.sprite(0, 0, 'bgmenu');
-    
+   create: function() {
+
+        this.game.add.sprite(0, 0, 'bgmenu');
+
+    }
 }
