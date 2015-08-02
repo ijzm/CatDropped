67813abaeec283efc1d8fab339631c135000d38a
ijzm <ignacio.zevallos@hotmail.com>
2015-08-02 01:05:23 -0500

    Need to randomize colors. Need to checl collisions Player-Cat

--- catdropped.js
+++ catdropped.js
@@ -3,8 +3,8 @@ var catdropped = function(game){
 	var player;
 }
 
-Cat = function(game, x, speed){
-	Phaser.Sprite.call(this, game, x, 0, "blue");
+Cat = function(game, x, speed, color){
+	Phaser.Sprite.call(this, game, x, 0, color);
 	game.physics.enable(this, Phaser.Physics.ARCADE);
 	this.ySpeed = speed;
 }
@@ -21,22 +21,37 @@ Cat.prototype.update = function() {
 	}
 }
 
+
 catdropped.prototype = {
 	create: function() {
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 		this.game.add.sprite(0, 0, 'bg');
 		player = this.game.add.sprite(10, 10, 'net');
 		
-			var cat = new Cat(this.game, 100, 50);
-			this.game.add.existing(cat);
-
-
+		
+		
+		
+		//DEBUG, TODO: REMOVE OR COMMENT
+		var debugbutton = this.game.add.button(0,0,"heart",this.createcat,this);
 	},
 
 	update: function() {
 		player.x = this.game.input.mousePointer.x - (player.width/2);
 		player.y = this.game.input.mousePointer.y - (player.height/2);
+		
+		this.game.physics.arcade.overlap(player, this.game.debugbutton, this.catchcat, null, this);
 
+	},
+	
+	createcat: function(){
+			console.log("DEBUG");
+			
+			var cat = new Cat(this.game, Math.floor(Math.random() * (this.game.width-42))  , Math.floor(Math.random() * 100) + 50  , "red");
+			this.game.add.existing(cat);
+	},
+	
+	catchcat: function(){
+		console.log("caught");
+		
 	}
-
 }
